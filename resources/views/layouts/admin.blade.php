e117cfa5c35ab866fa2dcd25496fd61fff83662c
Michael Lebon <lebontje45@hotmail.com>
2022-04-25 08:22:24 +0200

    2ste commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,7 +67,7 @@
                         Dashboard
                     </a>
 
-                    <!--                    start Users-->
+                    <!--start Users-->
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUsers"
                        aria-expanded="false" aria-controls="collapseUsers">
                         <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
@@ -81,84 +81,39 @@
                             <a class="nav-link" href="{{route('users.create')}}">Create User</a>
                         </nav>
                     </div>
-                    <!--                    end users-->
+                    <!--end users-->
 
-                    <!--                    start Post-->
+
+                    <!--start posts-->
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts"
                        aria-expanded="false" aria-controls="collapsePosts">
-                        <div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div>
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                         Posts
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
-                    <div class="collapse" id="collapsePosts" aria-labelledby="headingOne"
-                         data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="{{route('posts.index')}}">All Posts</a>
-                            <a class="nav-link" href="{{route('posts.create')}}">Create Post</a>
 
-                        </nav>
-                    </div>
-                    <!--                    end Post-->
-
-                    <!--                    start PostCategories-->
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                       data-bs-target="#collapsePostCategories"
-                       aria-expanded="false" aria-controls="collapsePostCategories">
-                        <div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div>
-                        PostCategories
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="collapsePostCategories" aria-labelledby="headingOne"
-                         data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="{{route('postCategories.index')}}">All PostCategories</a>
-                            <a class="nav-link" href="{{route('postCategories.create')}}">Create PostCategory</a>
-
-                        </nav>
-                    </div>
-                    <!--                    end PostCategories-->
-
-
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
-                       aria-expanded="false" aria-controls="collapsePages">
-                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                        Pages
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-
-                    <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
+                    <div class="collapse" id="collapsePosts" aria-labelledby="headingTwo"
                          data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                            <a class="nav-link" href="{{route('posts.index')}}">All Posts</a>
+                            <a class="nav-link" href="{{route('posts.create')}}">Create Post</a>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                               data-bs-target="#pagesCollapseAuth" aria-expanded="false"
-                               aria-controls="pagesCollapseAuth">
-                                Authentication
+                               data-bs-target="#collapsePostCategories" aria-expanded="false"
+                               aria-controls="collapsePostCategories">
+                                PostCategories
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
+                            <div class="collapse" id="collapsePostCategories" aria-labelledby="headingOne"
                                  data-bs-parent="#sidenavAccordionPages">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="login.html">Login</a>
-                                    <a class="nav-link" href="register.html">Register</a>
-                                    <a class="nav-link" href="password.html">Forgot Password</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                               data-bs-target="#pagesCollapseError" aria-expanded="false"
-                               aria-controls="pagesCollapseError">
-                                Error
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-                                 data-bs-parent="#sidenavAccordionPages">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="401.html">401 Page</a>
-                                    <a class="nav-link" href="404.html">404 Page</a>
-                                    <a class="nav-link" href="500.html">500 Page</a>
+                                    <a class="nav-link" href="{{route('postCategories.index')}}">All PostCategories</a>
+                                    <a class="nav-link" href="{{route('postCategories.create')}}">Create PostCategory</a>
                                 </nav>
                             </div>
                         </nav>
                     </div>
+                    <!-- end posts-->
+
                     <div class="sb-sidenav-menu-heading">Addons</div>
                     <a class="nav-link" href="charts.html">
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
@@ -172,7 +127,7 @@
             </div>
             <div class="sb-sidenav-footer">
                 <div class="small">Logged in as:</div>
-                Start Bootstrap
+                {{ Auth::user()->first_name}} {{ Auth::user()->last_name}}
             </div>
         </nav>
     </div>
